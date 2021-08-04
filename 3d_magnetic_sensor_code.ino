57cfd391db99dd030f88687a326fdfc1f5127c5e
Abdullah Muhammad Sadiq <abdullahsadiq7@yahoo.com>
2021-08-04 19:12:12 +0400

    Add files via upload

---
+++ 3d_magnetic_sensor_code.ino
@@ -0,0 +1,34 @@
+#include <Tle493d_w2b6.h>
+const float pi = (22.0 / 7.0);
+bool switchPressed;
+
+Tle493d_w2b6 Tle493dMagnetic3DSensor = Tle493d_w2b6();
+void setup() {
+  Serial.begin(9600);
+  Tle493dMagnetic3DSensor.begin();
+  Tle493dMagnetic3DSensor.begin();
+}
+
+void loop() {
+  Tle493dMagnetic3DSensor.updateData();
+  delay(100);
+  isSwitchPressed();
+
+  float angle = pi + Tle493dMagnetic3DSensor.getAzimuth();
+  angle = (angle * 180.0) / pi;
+
+  if (!switchPressed) {
+    Serial.println(angle);
+  } else if (switchPressed) {
+    //Do here whatever you want to do when the switch is pressed
+  }
+}
+
+void isSwitchPressed() {
+  if (Tle493dMagnetic3DSensor.getNorm() < 55) {
+    switchPressed = false;
+  }
+  else if (Tle493dMagnetic3DSensor.getNorm() > 85) {
+    switchPressed = true;
+  }
+}
---
+++ arduino_mkr1000_code_.ino
@@ -0,0 +1,82 @@
+float deg;
+int pinRed = 2;
+int pinGreen = 3;
+int pinBlue = 4;
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  pinMode(pinRed, OUTPUT);
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinBlue, OUTPUT);
+}
+
+void loop() {
+  if (Serial1.available() > 0) {
+    deg = Serial1.parseFloat();
+    setLedColorHSV(deg, 1, 1);
+  }
+}
+
+void setLedColorHSV(int h, double s, double v) {
+  //this is the algorithm to convert from RGB to HSV
+  double r = 0;
+  double g = 0;
+  double b = 0;
+
+  double hf = h / 60.0;
+
+  int i = (int)floor(h / 60.0);
+  double f = h / 60.0 - i;
+  double pv = v * (1 - s);
+  double qv = v * (1 - s * f);
+  double tv = v * (1 - s * (1 - f));
+
+  switch (i) {
+    case 0:
+      r = v;
+      g = tv;
+      b = pv;
+      break;
+    case 1:
+      r = qv;
+      g = v;
+      b = pv;
+      break;
+    case 2:
+      r = pv;
+      g = v;
+      b = tv;
+      break;
+    case 3:
+      r = pv;
+      g = qv;
+      b = v;
+      break;
+    case 4:
+      r = tv;
+      g = pv;
+      b = v;
+      break;
+    case 5:
+      r = v;
+      g = pv;
+      b = qv;
+      break;
+  }
+
+  //set each component to a integer value between 0 and 255
+  int red = constrain((int)255 * r, 0, 255);
+  int green = constrain((int)255 * g, 0, 255);
+  int blue = constrain((int)255 * b, 0, 255);
+
+  Serial.print("Red: ");    Serial.print(red);
+  Serial.print(" Green: "); Serial.print(green);
+  Serial.print(" Blue: ");   Serial.print(blue);
+  Serial.println();
+
+  analogWrite(pinRed, red);
+  analogWrite(pinGreen, green);
+  analogWrite(pinBlue, blue);
+}
